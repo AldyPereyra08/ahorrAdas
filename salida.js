d25062916c238e04f4d07f635d38a26300f2fc24
aldana08 <aldy.pereyra.desa@gmail.com>
2021-08-24 01:23:28 -0300

    agrego estilos a la tabla operaciones

--- salida.js
+++ salida.js
@@ -36,7 +36,16 @@ let operacionesData = [
 let operaciones = document.querySelector('#operaciones');
 
 operaciones.innerHTML = operacionesData.map(function (operacion) {
-	return `<div class="row"><div class="col">${operacion.nombre}</div><div class="col">${operacion.categoria}</div><div class="col">${operacion.fecha}</div><div class="col">${operacion.monto}</div><div class="col-md-4"><button type="submit" class="btn btn-primary">Editar</button><button type="submit" class="btn btn-primary">Eliminar</button></div></div>`;
+    return `<div class="row">
+    <div class="col">${operacion.nombre}</div>
+    <div class="col">${operacion.categoria}</div>
+    <div class="col">${operacion.fecha}</div>
+    <div class="col">${operacion.monto}</div>
+    <div class="col-md-4">
+    <button type="submit" class="btn btn-primary btn-editar-op">Editar</button>
+    <button type="submit" class="btn btn-primary btn-eliminar-op">Eliminar</button>
+    </div>
+    </div>`;
 });
 
 // const operaciones = operacionesData.map(function (operacion, index) {
