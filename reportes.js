db5ca5721ee152c8f119563bbb4882ce147369c7
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2021-08-25 21:35:11 -0300

    doceavo commit

--- reportes.js
+++ reportes.js
@@ -2,6 +2,6 @@ var monto1 = document.getElementById("montoPositivo");
 var monto2 = document.getElementById("montoNegativo");
 var montoTotal = document.getElementById("montoTotal");
 var restarMontos = function (monto1, monto2) {
-    montoTotal = monto1 - monto2;
+    montoTotal.innerText = (monto1 - monto2).toString();
 };
-restarMontos();
+restarMontos(parseInt(monto1.innerHTML), parseInt(monto2.innerHTML));
